5979624b2669e1a1681cdf495a3b081e51275c9c
RahulPatil <rahulrohidaspatil@gmail.com>
2025-12-21 23:58:41 +0100

    setup layout for visualization

---
+++ app/trapping-rain-water/page.tsx
@@ -0,0 +1,9 @@
+import RainWater from "@/components/rain-water"
+
+export default function Page() {
+  return (
+    <div className="flex h-screen flex-col">
+      <RainWater />
+    </div>
+  )
+}
---
+++ components/rain-water.tsx
@@ -0,0 +1,9 @@
+export default function RainWater() {
+  return (
+    <>
+      <div className="border-b text-center">Header</div>
+      <div className="flex flex-1 items-center justify-center">Content</div>
+      <div className="border-t text-center">Footer</div>
+    </>
+  )
+}
