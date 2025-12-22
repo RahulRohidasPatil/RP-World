6971333974766650c01913c08289da789baae5a3
RahulPatil <rahulrohidaspatil@gmail.com>
2025-12-22 17:18:05 +0100

    fix: update viewport import and adjust height class for TrappingRainWater page

--- app/trapping-rain-water/page.tsx
+++ app/trapping-rain-water/page.tsx
@@ -2,7 +2,7 @@ import TrappingRainWater from "@/components/trapping-rain-water"
 
 export default function Page() {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-dvh flex-col">
       <TrappingRainWater />
     </div>
   )
