88ec275e725b071370c28c2eadd64dd8807f3ab8
RahulPatil <rahulrohidaspatil@gmail.com>
2025-12-30 02:07:59 +0100

    add dark mode support

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,8 +1,12 @@
 import { Suspense } from "react"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Layout({ children }: LayoutProps<"/chat">) {
   return (
     <div className="mx-auto flex h-dvh w-full flex-col p-1 sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl 2xl:w-7xl">
+      <div className="text-center">
+        <ModeToggle />
+      </div>
       <Suspense>{children}</Suspense>
     </div>
   )
