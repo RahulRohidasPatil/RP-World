fc142684a148858e9ce0fa90a41ecc7b1bb1e757
RahulPatil <rahulrohidaspatil@gmail.com>
2025-12-30 02:14:58 +0100

    add gap between layout elements in chat component

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Layout({ children }: LayoutProps<"/chat">) {
   return (
-    <div className="mx-auto flex h-dvh w-full flex-col p-1 sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl 2xl:w-7xl">
+    <div className="mx-auto flex h-dvh w-full flex-col gap-1 p-1 sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl 2xl:w-7xl">
       <div className="text-center">
         <ModeToggle />
       </div>
